5406072a6d55372e455e20d4063e4e307e287f1e
Vlad <vldiark@gmail.com>
2024-07-24 13:01:50 +0300

    Fixes php 8.2

--- .provision/phpbrew/bashrc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export PHPBREW_HOME=/opt/phpbrew
-export PHPBREW_ROOT=/opt/phpbrew
-source /opt/phpbrew/bashrc
--- src/Yandex/Common/ObjectModel.php
+++ src/Yandex/Common/ObjectModel.php
@@ -5,54 +5,57 @@
  * @copyright NIX Solutions Ltd.
  * @link https://github.com/nixsolutions/yandex-php-library
  */
-
-/**
- * @namespace
- */
 namespace Yandex\Common;
+use Iterator;
+use Countable;
 
 /**
  * Class ObjectModel
  * @package Yandex\Common
  */
-class ObjectModel extends Model implements \Iterator, \Countable
+class ObjectModel extends Model implements Iterator, Countable
 {
-    protected $collection = [];
-    protected $innerCounter = -1;
+    protected array $collection = [];
+    protected int $innerCounter = -1;
 
-    public function current()
+    public function __construct(array $collection = [])
     {
-        if (is_array(current($this->collection))) {
-            return new ObjectModel(current($this->collection));
-        }
-
-        return current($this->collection);
+        $this->collection = $collection;
     }
 
-    public function next()
+    public function current(): mixed
+    {
+        $current = current($this->collection);
+        if (is_array($current)) {
+            return new ObjectModel($current);
+        }
+        return $current;
+    }
+
+    public function next(): void
     {
         $this->innerCounter++;
-        return next($this->collection);
+        next($this->collection);
     }
 
-    public function key()
+    public function key(): mixed
     {
         return key($this->collection);
     }
 
-    public function valid()
+    public function valid(): bool
     {
-        return $this->innerCounter < count($this->collection);
+        $key = key($this->collection);
+        return $key !== null && $key !== false;
     }
 
-    public function rewind()
+    public function rewind(): void
     {
         $this->innerCounter = 0;
         reset($this->collection);
-        return;
     }
 
-    public function count()
+    public function count(): int
     {
         return count($this->collection);
     }
